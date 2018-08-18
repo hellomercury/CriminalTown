eb4cd3a101b7017cdeaa3fef48c2c92e761b8ff3
IndianCoder666 <v.muhlisov@yandex.ru>
2018-08-18 23:22:51 +0300

    Quests editor upgrade

--- Assets/Editor/QuestsEditor/QuestsEditor.cs
+++ Assets/Editor/QuestsEditor/QuestsEditor.cs
@@ -12,12 +12,14 @@ namespace CriminalTown.Editors {
             }
         }
     }
-    
+
     public class QuestsEditor : EditorWindow {
         private static QuestsEditor m_instance;
         private static QuestsSerialization m_questsCollection;
-        private GUIStyle m_titleStyle;
-        private Vector2 m_scroll;
+        private static GUIStyle m_titleStyle;
+
+        private static Vector2 m_scroll;
+        private static int[] m_popups;
 
         [MenuItem("CriminalTown/Quests editor")]
         public static void ShowQuestsEditor() {
@@ -26,7 +28,8 @@ namespace CriminalTown.Editors {
                 Debug.LogError("You must open Map scene!\n(or scene which contains QuestsSerialization.cs script)");
                 return;
             }
-            
+
+            m_popups = new int[m_questsCollection.Quests.Count];
             if (m_instance == null) {
                 m_instance = GetWindow<QuestsEditor>();
             }
@@ -35,7 +38,7 @@ namespace CriminalTown.Editors {
 
         private void OnEnable() {
             m_scroll = Vector2.zero;
-            
+
             m_titleStyle = new GUIStyle() {
                 alignment = TextAnchor.UpperCenter,
                 fontSize = 25,
@@ -45,43 +48,54 @@ namespace CriminalTown.Editors {
         }
 
         private void OnDisable() {
-            
+
         }
 
         private void OnGUI() {
-            int verticalPosition = 0;
             SerializedObject questsObject = new SerializedObject(m_questsCollection);
             SerializedProperty questsProperty = questsObject.FindProperty("m_quests");
-            
+
             GUILayout.Label("Quests", m_titleStyle);
-            verticalPosition += 30;
             m_scroll = GUILayout.BeginScrollView(m_scroll);
             {
                 for (int i = 0; i < questsProperty.arraySize; i++) {
-                    SerializedProperty quest = questsProperty.GetArrayElementAtIndex(i);
-                    EditorGUILayout.BeginVertical(EditorStyles.helpBox);
-                    EditorGUILayout.PropertyField(quest, true);
-                    verticalPosition += (int)EditorGUI.GetPropertyHeight(quest, true);
-                    //GUILayout.
-                    if (GUILayout.Button("Remove quest")) {
-                        if (EditorUtility.DisplayDialog("Delete quest", "A you sure you want to delete quest: questId", "Yes", "No")) {
-                            m_questsCollection.Quests.RemoveAt(i);
-                        }
-                    }
-                    EditorGUILayout.EndVertical();
+                    ManageQuest(questsProperty.GetArrayElementAtIndex(i), i);
                     GUILayout.Space(30);
                 }
             }
             GUILayout.EndScrollView();
-            
+
             GUILayout.BeginHorizontal();
-//            if (GUILayout.Button("Save (doesn't work yet)")) {
-//                
-//            }
+//          if (GUILayout.Button("Save (doesn't work yet)")) {
+//              
+//          }
             if (GUILayout.Button("Add quest")) {
                 m_questsCollection.Quests.Add(new Quest());
             }
             GUILayout.EndHorizontal();
+
+            questsObject.ApplyModifiedProperties();
+        }
+
+        private void ManageQuest(SerializedProperty quest, int index) {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            m_popups[index] = EditorGUILayout.Popup(m_popups[index], new string[] {
+                "RobberyQuest",
+                "ItemQuest",
+                "StatsUpCharacterQuest",
+                "LevelUpCharacterQuest",
+                "StatusCharacterQuest",
+                "KickCharacterQuest",
+                "HireCharacterQuest",
+                "ChoiceQuest",
+            });
+            EditorGUILayout.PropertyField(quest, true);
+            if (GUILayout.Button("Remove quest")) {
+                if (EditorUtility.DisplayDialog("Delete quest", "A you sure you want to delete quest: questId", "Yes", "No")) {
+                    m_questsCollection.Quests.RemoveAt(index);
+                }
+            }
+            EditorGUILayout.EndVertical();
         }
 
     }
